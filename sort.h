ab748791910a5b844f04ab3ce619264d55d85185
BraveScorpion <naction923@gmail.com>
2023-06-26 02:33:08 -0700

    0-bubble_sort.c

--- sort.h
+++ sort.h
@@ -17,4 +17,7 @@ typedef struct listint_s
 } listint_t;
 
 void bubble_sort(int *array, size_t size);
+void print_list(const listint_t *list);
+void print_array(const int *array, size_t size);
+
 #endif
