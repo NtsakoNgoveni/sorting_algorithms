d33e3774d5905e673f712c1188e5b845d903828b
BraveScorpion <naction923@gmail.com>
2023-06-27 02:30:12 -0700

    1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * insertion_sort_list- sorts a doubly linked list via insertion sort
+ * @list: is the head of the list
+ * Return: void
+ */
+
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *ptr, *curr, *prev;
+
+	if (!list || !(*list))
+		return;
+
+	ptr = *list;
+
+	while (ptr)
+	{
+		curr = ptr;
+
+		while (ptr->prev)
+		{
+			if (ptr->n < ptr->prev->n)
+			{
+				prev = ptr->prev;
+				prev->next = ptr->next;
+				ptr->next = prev;
+				ptr->prev = prev->prev;
+				prev->prev = ptr;
+				print_list(*list);
+			}
+		}
+		ptr = curr->next;
+	}
+}
